f19831119b4b0134b0cb5b80fc21b7967ea89449
Nuril Qomariyah Fajrin <132538594+nurilqf@users.noreply.github.com>
2023-07-11 18:29:58 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+# main Python app
+import streamlit as st
+import streamlit.components.v1 as stc
+
+from ml_app import run_ml_app
+
+def main():
+    st.title("Main App")
+    # stc.html(html_temp)
+
+    menu = ["Home", "Machine Learning"]
+    choice = st.sidebar.selectbox("Menu", menu)
+
+    if choice == "Home":
+        st.subheader("Home")
+        # st.markdown(desc_temp, unsafe_allow_html=True)
+    elif choice == "Machine Learning":
+        st.subheader("Machine Learning Testing")
+        run_ml_app()
+
+if __name__ == '__main__':
+    main()
