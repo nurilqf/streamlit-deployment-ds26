6c7907aab4d5b55002cb45bd7797d6352f5ea1b9
Nuril Qomariyah Fajrin <132538594+nurilqf@users.noreply.github.com>
2023-07-11 18:44:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit.components.v1 as stc
 from ml_app import run_ml_app
 
 def main():
-    st.title("Main App")
+    st.title("HR Machine Learning Deployment")
     # stc.html(html_temp)
 
     menu = ["Home", "Machine Learning"]
